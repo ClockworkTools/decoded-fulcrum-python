becfc43f3fd677f4bef5c1aa35432bbc8d7c0dbe
keithhannon <keith@clockwork.co.nz>
2015-07-15 12:56:25 +1200

    Fixed another bug with photos being recoded

--- decodedFulcrum/fulcrumJsonUtils.py
+++ decodedFulcrum/fulcrumJsonUtils.py
@@ -121,8 +121,9 @@ def _recode(jsonFormValuesField, schema):
         del jsonFormValuesField[fieldName]
 
         # process any children of the added field
-        if isinstance(fieldValue, list):
+        fieldType = schema.getFieldType(fieldName)
 
+        if fieldType == 'Repeatable':
             # fieldValue is a list of child records
             for childRecord in fieldValue:
                 _recode(childRecord['form_values'], schema)
